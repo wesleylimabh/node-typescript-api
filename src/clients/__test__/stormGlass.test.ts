b3b0bd17d7d2608235da16581642907d7abd4a99
Wesley Lima <wesleylima@gmx.com>
2020-11-30 15:44:56 -0300

    add test should exclude incomplete data points

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -5,9 +5,8 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 
 jest.mock('axios');
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
-    const mockedAxios = axios as jest.Mocked<typeof axios>;
-
     const lat = -33.792726;
     const lng = 151.289824;
 
@@ -17,5 +16,26 @@ describe('StormGlass client', () => {
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+
+  it('should exclude incomplete data points', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+    const incompleteResponse = {
+      hours: [
+        {
+          windDirection: {
+            noaa: 300,
+          },
+          time: '2020-04-26T00:00:00+00:00',
+        },
+      ],
+    };
+    mockedAxios.get.mockResolvedValue({ data: incompleteResponse });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual([]);
   });
 });
