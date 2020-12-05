7acde1bc7383c446219cec13791129d04f35bf08
Wesley Lima <wesleylima@gmx.com>
2020-12-05 20:40:06 -0300

    add lib mongoose and setup database

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import { Application } from 'express';
 
 import { ForecastController } from './controller/forecast';
 import './util/module-alias';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -22,6 +24,14 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
