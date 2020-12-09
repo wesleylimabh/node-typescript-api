a5428598c8a46a07c7ae4965db789b920b1c6f98
Wesley Lima <wesleylima@gmx.com>
2020-12-08 22:51:32 -0300

    integrating beach and forecast and mocking APIs with nock

--- src/controller/forecast.ts
+++ src/controller/forecast.ts
@@ -1,50 +1,22 @@
 import { Controller, Get } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
+import { Forecast } from '@src/services/forecast';
 
+const forecast = new Forecast();
 @Controller('forecast')
 export class ForecastController {
   @Get('')
-  public getForecastForLoggedUser(_: Request, res: Response): void {
-    const body = [
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 2,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-          },
-        ],
-      },
-    ];
-    res.send(body);
+  public async getForecastForLoggedUser(
+    _: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const beaches = await Beach.find({});
+      const forecasteData = await forecast.processForecastForBeaches(beaches);
+      res.status(200).send(forecasteData);
+    } catch (error) {
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
