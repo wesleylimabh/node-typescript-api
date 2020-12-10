03430526d3189dff052f50d0358b5ebd13ffae03
Wesley Lima <wesleylima@gmx.com>
2020-12-09 23:35:01 -0300

    fix module-alias and add startup app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('index.ts');
+import 'module-alias/register';
+import { SetupServer } from './server';
+import config from 'config';
+
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
